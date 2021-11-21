1d7db5b863f6cfa3497ef45c4d2103154946b2b8
Cory Martin <cory.martin@dozensoft.com>
2021-11-20 20:50:13 -0600

    initial attempt at sendAlerts

--- cdk/assets/lambda/dynamodbstreams/app.ts
+++ cdk/assets/lambda/dynamodbstreams/app.ts
@@ -1,28 +1,44 @@
 import { DynamoDBStreamEvent } from "aws-lambda";
+import * as AWS from "aws-sdk";
+import { QueryInput } from "aws-sdk/clients/dynamodb";
 import { sendAlerts } from "./sendalerts";
+const ddb = new AWS.DynamoDB({ apiVersion: "2018-05-29" });
 
 export const handler = async (event: DynamoDBStreamEvent): Promise<any> => {
   console.log("DynamoDb Streams", JSON.stringify(event));
-  const { Records } = event;
-  for (let iterator = 0; iterator < Records.length; iterator++) {
-    const row = Records[iterator];
-    if (row.dynamodb && row.dynamodb.Keys && row.dynamodb.Keys.PK.S) {
-      const PK = row.dynamodb.Keys.PK.S;
-      const rowType = PK.substring(
-        0,
-        PK.indexOf("#") !== -1 ? PK.indexOf("#") : 999
-      );
-      console.log({ rowType });
-      switch (rowType) {
-        case "user":
-          break;
-        case "class":
-          sendAlerts(row);
-          break;
-        case "alert":
-          break;
-        default:
-        // do nothing
+  const TableName = process.env.tablename!;
+  const alertsQueryInput: QueryInput = {
+    TableName,
+    KeyConditionExpression: "GSI1PK = :PK",
+    ExpressionAttributeValues: {
+      ":PK": { S: `alert` },
+    },
+    IndexName: "GSI1",
+  };
+  const alertsQueryResult = await ddb.query(alertsQueryInput).promise();
+  console.log("alertsQueryResult", JSON.stringify(alertsQueryResult));
+  if (alertsQueryResult.Items && alertsQueryResult.Items.length > 0) {
+    const { Records } = event;
+    for (let iterator = 0; iterator < Records.length; iterator++) {
+      const row = Records[iterator];
+      if (row.dynamodb && row.dynamodb.Keys && row.dynamodb.Keys.PK.S) {
+        const PK = row.dynamodb.Keys.PK.S;
+        const rowType = PK.substring(
+          0,
+          PK.indexOf("#") !== -1 ? PK.indexOf("#") : 999
+        );
+        console.log({ rowType });
+        switch (rowType) {
+          case "user":
+            break;
+          case "class":
+            sendAlerts(row, alertsQueryResult.Items);
+            break;
+          case "alert":
+            break;
+          default:
+          // do nothing
+        }
       }
     }
   }
--- cdk/assets/lambda/dynamodbstreams/sendalerts.ts
+++ cdk/assets/lambda/dynamodbstreams/sendalerts.ts
@@ -1,8 +1,64 @@
 import { DynamoDBRecord } from "aws-lambda";
 import { emailTemplate, EmailTemplateInterface } from "./emailtemplate";
 import * as AWS from "aws-sdk";
-export const sendAlerts = async (row: DynamoDBRecord): Promise<any> => {
+export const sendAlerts = async (
+  row: DynamoDBRecord,
+  alerts: AWS.DynamoDB.ItemList
+): Promise<any> => {
   //
+  for (let iterator = 0; iterator > alerts.length; iterator++) {
+    const alert = alerts[iterator];
+    const joinParametersWith = alert.joinParametersWith
+      .S as AlertJoinParametersWithType;
+    if (alert.parameters.S && row.dynamodb && row.dynamodb.NewImage) {
+      const parameters = JSON.parse(alert.parameters.S) as AlertParameter[];
+      console.log({ parameters });
+      let sendAlert = joinParametersWith === "or" ? false : true;
+      for (let iterator = 0; iterator < parameters.length; iterator++) {
+        const parameter = parameters[iterator];
+        switch (parameter.parameterType) {
+          case "sessionType":
+            if (row.dynamodb?.NewImage.sessionType.S) {
+              const sessionType = JSON.parse(
+                row.dynamodb?.NewImage.sessionType.S
+              ) as any;
+              if (
+                sessionType.name === parameter.parameterData &&
+                joinParametersWith === "or"
+              ) {
+                sendAlert = true;
+              }
+              if (
+                sessionType.name !== parameter.parameterData &&
+                joinParametersWith === "and"
+              ) {
+                sendAlert = false;
+              }
+              break;
+            }
+        }
+      }
+      if (sendAlert) {
+        const params: EmailTemplateInterface = {
+          body: `<h1>Reinvent Catalog Alert</h1><p>alert: ${JSON.stringify(
+            alert
+          )}</p><p>session: ${JSON.stringify(row)}</p>`,
+          buttonHref: "",
+          buttonText: "",
+          footerHtml: "FOOTER",
+          header: "HEADER",
+          logoHref: "",
+          showButton: false,
+        };
+        await sendEmail(
+          params,
+          process.env.emailAddress!,
+          alert.emailAddress.S!,
+          "Reinvent Catalog Alert"
+        );
+      }
+    }
+  }
 };
 
 const sendEmail = async (
@@ -32,3 +88,37 @@ const sendEmail = async (
   };
   return await sesClient.sendEmail(sendEmailRequest).promise();
 };
+
+// copied from src/interfaces.ts
+// TODO: this should be stored in a separate location so that both files can reference the same interfaces
+export type AlertType = "sns" | "autoregister";
+export type AlertJoinParametersWithType = "and" | "or";
+export type AlertUpdateOrNewType = "update" | "new" | "both";
+
+export type AlertParameterType =
+  | "sessionIdEquals"
+  | "sessionIdStartsWith"
+  | "time"
+  | "sessionType";
+export interface DbAlert {
+  PK: string;
+  SK: string;
+  alertType: AlertType;
+  parameters: string;
+}
+//{ "classId": { "startswith": "svs"}, "datetime": { "after": "2021-11-29 09:00" }, "datetime: { "before": "2021-11-29 13:00"} }
+export interface TimeParameters {
+  before?: String;
+  after?: String;
+}
+export interface AlertParameter {
+  parameterType?: AlertParameterType;
+  parameterData?: string | TimeParameters;
+}
+export interface Alert {
+  id: string;
+  alertType: AlertType;
+  updateOrNew: AlertUpdateOrNewType;
+  parameters: AlertParameter[];
+  joinParametersWith: AlertJoinParametersWithType;
+}
