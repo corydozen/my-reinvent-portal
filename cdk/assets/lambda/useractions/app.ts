749f66536933fd21037c34286ec8aaf802e6d35a
Cory Martin <cory.martin@dozensoft.com>
2021-11-23 20:44:15 -0600

    add overview entry in db, add getEverybodysSchedule query to schema

--- cdk/assets/lambda/useractions/app.ts
+++ cdk/assets/lambda/useractions/app.ts
@@ -1,15 +1,21 @@
 import * as AWS from "aws-sdk";
 import { PostAuthenticationTriggerEvent } from "aws-lambda";
-import { PutItemInput } from "aws-sdk/clients/dynamodb";
+import { PutItemInput, QueryInput } from "aws-sdk/clients/dynamodb";
 
 const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+interface Person {
+  email: string;
+  sub: string;
+}
 
 export const createuser = async (
   event: PostAuthenticationTriggerEvent
 ): Promise<PostAuthenticationTriggerEvent> => {
   console.log(JSON.stringify(event));
+  const TableName = process.env.tablename!;
   const params: PutItemInput = {
-    TableName: process.env.tablename!,
+    TableName,
     Item: {
       PK: { S: `user#${event.request.userAttributes.sub}` },
       SK: { S: "info" },
@@ -20,5 +26,41 @@ export const createuser = async (
     },
   };
   await ddb.putItem(params).promise();
+  const getOverviewQueryInput: QueryInput = {
+    TableName,
+    KeyConditionExpression: "PK = :PK",
+    ExpressionAttributeValues: {
+      ":PK": { S: "overview" },
+    },
+  };
+  const overviewQueryResult = await ddb.query(getOverviewQueryInput).promise();
+  console.log({ overviewQueryResult });
+  let everybody: Person[] = [];
+  const me: Person = {
+    email: event.request.userAttributes.email,
+    sub: event.request.userAttributes.sub,
+  };
+  if (
+    overviewQueryResult.Items &&
+    overviewQueryResult.Items.length > 0 &&
+    overviewQueryResult.Items[0].everybody.S
+  ) {
+    // There will only be one if there are any
+    everybody = JSON.parse(
+      overviewQueryResult.Items[0].everybody.S
+    ) as Person[];
+    everybody.push(me);
+  } else {
+    everybody = [me];
+  }
+  const putOverviewParams: PutItemInput = {
+    TableName,
+    Item: {
+      PK: { S: "overview" },
+      everybody: { S: JSON.stringify(everybody) },
+      lastUpdated: { S: new Date().toString() },
+    },
+  };
+  await ddb.putItem(putOverviewParams).promise();
   return event;
 };
