dd10adcbd3f413d90318cc996f8e5cd0605cd02a
Cory Martin <cory.martin@dozensoft.com>
2021-11-28 14:10:19 -0600

    only alert on new sessions

--- cdk/assets/lambda/dynamodbstreams/sendalerts.ts
+++ cdk/assets/lambda/dynamodbstreams/sendalerts.ts
@@ -12,13 +12,13 @@ export const findAlertsToSend = async (
   alertsToSend: AlertToSend[],
   eventName: "INSERT" | "MODIFY" | "REMOVE"
 ): Promise<AlertToSend[]> => {
-  console.log({
-    row: JSON.stringify(row),
-    alerts: JSON.stringify(alerts),
-    alertsToSend: JSON.stringify(alertsToSend),
-    eventName,
-    alertsLength: alerts.length,
-  });
+  // console.log({
+  //   row: JSON.stringify(row),
+  //   alerts: JSON.stringify(alerts),
+  //   alertsToSend: JSON.stringify(alertsToSend),
+  //   eventName,
+  //   alertsLength: alerts.length,
+  // });
   for (let iterator = 0; iterator < alerts.length; iterator++) {
     const alert = alerts[iterator];
     console.log({ alert });
@@ -30,79 +30,80 @@ export const findAlertsToSend = async (
       const startTime = parseInt(row.dynamodb.NewImage.startTime.N!);
       const parameters = JSON.parse(alert.parameters.S) as AlertParameter[];
       console.log({ parameters });
-      // if (eventName === "INSERT") {
-      let sendAlert = true;
-      for (let iterator = 0; iterator < parameters.length; iterator++) {
-        const parameter = parameters[iterator];
-        switch (parameter.parameterType) {
-          case "sessionType":
-            if (sessionType.name !== parameter.parameterData) {
-              sendAlert = false;
-            }
-            break;
-          case "sessionIdEquals":
-            if (sessionId !== parameter.parameterData) {
-              sendAlert = false;
-            }
-            break;
-          case "sessionIdStartsWith":
-            if (
-              sessionId.substring(
-                0,
-                (parameter.parameterData as string).length
-              ) !== parameter.parameterData
-            ) {
-              sendAlert = false;
-            }
-            break;
-          case "time":
-            const eightHours = 28800000;
-            // I'm going to ignore the 'or' case for now
-            const { before, after } = parameter.parameterData as TimeParameters;
-            if (before) {
-              const beforeEpoch = parseInt(before) - eightHours;
-              if (beforeEpoch > startTime) {
+      if (eventName === "INSERT") {
+        let sendAlert = true;
+        for (let iterator = 0; iterator < parameters.length; iterator++) {
+          const parameter = parameters[iterator];
+          switch (parameter.parameterType) {
+            case "sessionType":
+              if (sessionType.name !== parameter.parameterData) {
                 sendAlert = false;
               }
-            }
-            if (after) {
-              const afterEpoch = parseInt(after) - eightHours;
-              if (afterEpoch < startTime) {
+              break;
+            case "sessionIdEquals":
+              if (sessionId !== parameter.parameterData) {
                 sendAlert = false;
               }
-            }
-            break;
-          default:
-            sendAlert = false;
+              break;
+            case "sessionIdStartsWith":
+              if (
+                sessionId.substring(
+                  0,
+                  (parameter.parameterData as string).length
+                ) !== parameter.parameterData
+              ) {
+                sendAlert = false;
+              }
+              break;
+            case "time":
+              const eightHours = 28800000;
+              // I'm going to ignore the 'or' case for now
+              const { before, after } =
+                parameter.parameterData as TimeParameters;
+              if (before) {
+                const beforeEpoch = parseInt(before) - eightHours;
+                if (beforeEpoch > startTime) {
+                  sendAlert = false;
+                }
+              }
+              if (after) {
+                const afterEpoch = parseInt(after) - eightHours;
+                if (afterEpoch < startTime) {
+                  sendAlert = false;
+                }
+              }
+              break;
+            default:
+              sendAlert = false;
+          }
         }
-      }
-      console.log({ sendAlert });
-      if (sendAlert) {
-        const emailIndex = alertsToSend.findIndex(
-          e => e.emailAddress === alert.emailAddress.S
-        );
-        if (emailIndex === -1) {
-          alertsToSend.push({
-            body: `<h1>Reinvent Catalog Alert</h1><p>Name: ${
+        console.log({ sendAlert });
+        if (sendAlert) {
+          const emailIndex = alertsToSend.findIndex(
+            e => e.emailAddress === alert.emailAddress.S
+          );
+          if (emailIndex === -1) {
+            alertsToSend.push({
+              body: `<h1>Reinvent Catalog Alert</h1><p>Name: ${
+                row.dynamodb.NewImage.name.S
+              }</p><p>SessionType: ${sessionType.name}</p><p>Description: ${
+                row.dynamodb.NewImage.description.S
+              }</p><p>session: ${JSON.stringify(
+                row
+              )}</p><p>alert: ${JSON.stringify(alert)}</p>`,
+              emailAddress: alert.emailAddress.S!,
+            });
+          } else {
+            alertsToSend[emailIndex].body += `<hr/><p>Name: ${
               row.dynamodb.NewImage.name.S
             }</p><p>SessionType: ${sessionType.name}</p><p>Description: ${
               row.dynamodb.NewImage.description.S
             }</p><p>session: ${JSON.stringify(
               row
-            )}</p><p>alert: ${JSON.stringify(alert)}</p>`,
-            emailAddress: alert.emailAddress.S!,
-          });
-        } else {
-          alertsToSend[emailIndex].body += `<hr/><p>Name: ${
-            row.dynamodb.NewImage.name.S
-          }</p><p>SessionType: ${sessionType.name}</p><p>Description: ${
-            row.dynamodb.NewImage.description.S
-          }</p><p>session: ${JSON.stringify(row)}</p><p>alert: ${JSON.stringify(
-            alert
-          )}</p>`;
+            )}</p><p>alert: ${JSON.stringify(alert)}</p>`;
+          }
         }
       }
-      // }
     }
   }
   console.log({ alertsToSend });
